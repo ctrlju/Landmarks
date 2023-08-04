ef6ba1040d28686daf002b4493f75eca193fb198
C1327991 Juliana Ferreira de Lima Pereira <c1327991@interno.bb.com.br>
2023-08-04 10:22:52 -0300

    Initial Commit

---
+++ Landmarks/CircleImage.swift
@@ -0,0 +1,25 @@
+//
+//  CircleImage.swift
+//  Landmarks
+//
+//  Created by juliana on 04/08/23.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay {
+                Circle().stroke(.white, lineWidth: 3)
+            }
+            .shadow(radius: 5)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
